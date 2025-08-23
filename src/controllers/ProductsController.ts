49a5f1e4876f519a9b06151441e2c7730b39b2d1
Guilherme Moreno <guilhermemoreno200@gmail.com>
2025-08-23 09:31:30 -0300

    iniciando banco de dados

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import AppError from "../utils/AppError";
 import { z } from "zod";
+
 export class ProductsController {
   index(request: Request, response: Response) {
     const { page, limit } = request.query;
@@ -12,8 +13,13 @@ export class ProductsController {
 
   create(request: Request, response: Response) {
     const bodySchema = z.object({
-      name: z.string(),
-      price: z.number(),
+      name: z.string({ required_error: "Nome é obrigatório!" }).trim().min(6, {
+        message: "Nome deve ter pelo menos 6 caracteres!",
+      }),
+      price: z
+        .number({ required_error: "Preço é obrigatório!" })
+        .positive({ message: "Preço não pode ser negativo!" })
+        .gte(10, { message: "Preço deve ser maior que 10!" }),
     });
 
     const { name, price } = bodySchema.parse(request.body); // Se der erro, o zod vai lançar um erro
